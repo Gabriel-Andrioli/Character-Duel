0c2e5fb3ecb3ee0a0cf604ff612e7bbca3b24b8f
PedroMiana <pedromianaamaral@gmail.com>
2025-06-11 22:06:14 -0300

    Update Main.java

--- src/main/java/com/characterduel/Main.java
+++ src/main/java/com/characterduel/Main.java
@@ -1,5 +1,8 @@
 package com.characterduel;
 
+//Pedro Miana Amaral - 202465511B
+//Gabriel Andrioli Fonseca - 202465502B
+
 public class Main {
     public static void main(String[] args) {
         Game.play();
